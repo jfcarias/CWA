acef5ee998c91db7084c7a289c5d89f674891c0a
mikelcasals <mikelcasalsbaena@gmail.com>
2021-09-03 19:08:01 +0200

    connected login to db

--- routes/index.js
+++ routes/index.js
@@ -1,30 +1,148 @@
+//copiado
 var express = require('express');
 var router = express.Router();
+const pg = require('pg');
+//const path = require('path');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+/*
+const config = {
+					connectionString: process.env.DATABASE_URL,
+					ssl: true,
+					connectionTimeoutMillis: 5000,
+					max: 15
+				};
 
+*/
+
+const pool = new pg.Pool({
+
+	user: 'postgres',
+  
+	host: 'localhost',
+  
+	database: 'CWA',
+  
+	password: 'cwa',
+  
+	port: 5432,
+  
+})
+
+const secret = process.env.secret;
+//const pool = new pg.Pool(config);
+
+//copiado
 router.get('/', function(req, res, next) {
   res.render('login');
 });
-router.post('/login', function(req, res, next) {
-  var username = req.body.username;
-  var password = req.body.password;
-  console.log(username+password);
-  var u = {
-  	username: "prueba",
-  	password: "prueba"
-  }
-  if (username == u.username && password == u.password) {
-      res.render('menu');
-  }
-  else if (username == "admin" && password == "admin") {
-      res.render('menuadministrador');
-  }
-  else {
-      res.render('loginerror');
-  }
+
+
+//copiado
+router.post('/login', (req, res, next) =>{
+	const data = {email: req.body.email, password: req.body.password};
+	pool.connect((err, client, done)=>{
+		if(err){
+			console.log(err);
+			res.render('loginerror');
+		}else{
+			client.query('select usuario, password, nombre, apellidos, email from voluntarios where email=$1;', [data.email], (err, result)=>{
+
+				console.log(data)
+				done();
+				if(err){
+					console.log(err);
+				}
+				if(!result){
+					res.render('loginerror', {wrong: true});
+				}
+
+				console.log(result)
+				
+				/*
+				if(result.rows[0].email !== 'admin'){
+					res.render('menuadministrador', {wrong: true});
+				}
+						
+				if(result.rows[0].email == 'admin'){
+					res.render('menuadministrador', {wrong: true});
+				}
+				*/
+				user = {usuario: result.rows[0].usuario, email: result.rows[0].email, password: result.rows[0].password, nombre:result.rows[0].nombre, apellidos: result.rows[0].apellidos};
+
+				
+				bcrypt.compare(data.password, user.password, (err, isMatch)=>{
+					if(err){
+						console.log(err);
+					}else{
+						if(isMatch){
+							jwt.sign({user:user}, secret, {expiresIn: 24*60*60}, (err, token)=>{
+								if(err){
+									console.log(err);
+								}else{
+									res.cookie('auth',token);
+									res.redirect('/admin/admin_functions');
+								}
+							});
+						}else{
+							if (user.email == 'admin'){
+								res.render('menuadministrador', {wrong: true});
+							}else{
+								res.render('menu', {wrong: true});
+							}
+							
+						}
+					}	
+				});
+				
+			});
+		}		
+	});
 });
+//copiado
+
 router.get('/menu', function (req, res, next) {
     res.render('menu');
 });
+
+router.get('/menuadministrador/dardealta', function (req, res, next) {
+    res.render('menuadministrador/dardealta');
+});
+//copiado
+router.post('/menuadministrador/dardealta', function(req, res, next){
+	var data = {name: req.body.name, Email: req.body.Email, H_Password: req.body.H_Password};
+	
+	// hasshing the password
+	bcrypt.genSalt(10, (err, salt)=>{
+		bcrypt.hash(data.H_Password, salt, (err, hash)=>{
+			if(err){
+				console.log(err)
+				res.render('menuadministrador/dardealta', {message: 'Ha habido un problema interno, por favor vuelva a enviar su información. Disculpe las molestias.'})
+			}else{
+				data.H_Password = hash;
+			}
+		});
+	});
+
+	//inserting the user
+	pool.connect(function(err, client, done){
+		if(err) {
+			console.log(err);
+			return res.status(500).json({success: false, data: err});
+		}
+
+		client.query('insert into Voluntarios(Nombre, Apellidos, H_Password, Email) values($1, $2, $3, $4) returning Usuario;',
+		[data.name, data.username, data.password], function(err, result){
+			done();
+			if(err){
+				res.render('menuadministrador/dardealta', {used: true})
+			}else{
+				res.render('menuadministrador', {message: {positive:true, message:'You have been successfully registered. You can now access with your username and password.'}});
+			}
+		});
+	});
+});
+//copiado
 router.get('/menuadministrador', function (req, res, next) {
     res.render('menuadministrador');
 });
